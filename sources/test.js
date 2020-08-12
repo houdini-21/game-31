bfe630c9166d563f62e0ba9e2e905f7b640ce158
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-08-11 20:57:46 -0600

    generando cartas correctamente, proximamente suma puntos

--- sources/test.js
+++ sources/test.js
@@ -2,6 +2,7 @@ let masobarajeado = false;
 let masoDeCartas;
 let cartashumano = [];
 let cartascpu = [];
+
 class Cartas {
   constructor() {
     this._maso = [];
@@ -21,7 +22,7 @@ class Cartas {
       cartasparuser.push(cartauser);
       masoDeCartas.splice(cardAleatoria, 1);
     }
-    this.masocadajugador(cartasparuser, player);
+    this.entregarCartaJugador(cartasparuser, player);
   }
 
   armarMasoDeCartas() {
@@ -47,14 +48,41 @@ class Cartas {
     });
   }
 
-  masocadajugador(masocartas, player) {
-    if (player === 'cpu') {
+  entregarCartaJugador(masocartas, player) {
+    if (player === "cpu") {
       masocartas.forEach((cartas) => {
         cartascpu.push(cartas);
+        this.generarTemplatedeCartas(cartascpu, player);
       });
     } else {
       masocartas.forEach((cartas) => {
         cartashumano.push(cartas);
+        this.generarTemplatedeCartas(cartashumano, player);
+      });
+    }
+  }
+  
+  generarTemplatedeCartas(cartasarray, playeruid) {
+    let result = "";
+    if (playeruid === "cpu") {
+      cartasarray.forEach(() => {
+        const carta = `             
+        <div class="carta-mesa back-card">
+          <i class="fal fa-helmet-battle logo-card-small"></i>
+        </div>`;
+        result += carta;
+        document.getElementById("banquero").innerHTML = result;
+      });
+    } else {
+      cartasarray.forEach((data) => {
+        const template = `
+        <div class="carta-mesa front-card ${data.tipo}-card">
+          <div class="number-top"><p>${data.valor}</p></div>
+            <i class="fal fa-${data.tipo}"></i>
+          <div class="number-bottom"><p>${data.valor}</p></div>
+        </div>`;
+        result += template;
+        document.getElementById("player").innerHTML = result;
       });
     }
   }
@@ -65,6 +93,5 @@ let player = new Cartas().tomarcarta(3, "houdini");
 
 document.getElementById("tomar").addEventListener("click", () => {
   player = new Cartas().tomarcarta(1, "houdini");
-
-  console.log(cartashumano);
+  cpu = new Cartas().tomarcarta(1, "cpu");
 });
