2c5e5c5410905db3ebb2f3966dde8130fd75c960
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-08-14 10:38:18 -0600

    cambio de planes en turnos de jugador

--- sources/test.js
+++ sources/test.js
@@ -5,51 +5,6 @@ let cartascpu = [];
 let puntoshumano = 0;
 let puntoscpu = 0;
 
-class Marcador {
-  calcularpuntos(puntos, player) {
-    let puntajegeneral = puntos[puntos.length - 1].valor;
-    switch (puntajegeneral) {
-      case "Q":
-        puntajegeneral = 10;
-        break;
-      case "J":
-        puntajegeneral = 10;
-        break;
-      case "K":
-        puntajegeneral = 10;
-        break;
-      case "AS":
-        //aqui debo de preguntar por el valor a darle a 11
-        if (player === "cpu") {
-          puntajegeneral = 11;
-        } else {
-          while (!(puntajegeneral === 11 || puntajegeneral === 1)) {
-            let valoras = parseInt(
-              prompt("sacaste un AS elije su valor solo puede ser de 1 a 11")
-            );
-            puntajegeneral = valoras;
-          }
-        }
-        break;
-    }
-    player === "cpu"
-      ? (puntoscpu += puntajegeneral)
-      : (puntoshumano += puntajegeneral);
-    console.log(puntoscpu, puntoshumano);
-  }
-
-  detectarganador() {}
-
-  reiniciarmarcador() {
-    puntoshumano = 0;
-    puntoscpu = 0;
-  }
-}
-
-//reordenamiento de codigo y dejando camino para la logica del banquero
-//problemas a la hora de asignar un valor al as
-//solucionar el valor de as que pregunte el valor al user y al banquero(elija segun situacion)
-
 class Ui {
   generarTemplatedeCartas(cartasarray, playeruid) {
     let result = "";
@@ -77,19 +32,57 @@ class Ui {
   }
 }
 
+class Marcador {
+  calcularpuntos(puntos, player) {
+    let puntajegeneral = puntos[puntos.length - 1].valor;
+    switch (puntajegeneral) {
+      case "Q":
+        puntajegeneral = 10;
+        break;
+      case "J":
+        puntajegeneral = 10;
+        break;
+      case "K":
+        puntajegeneral = 10;
+        break;
+      case "AS":
+        if (player === "cpu") {
+          puntajegeneral = 11;
+        } else {
+          while (!(puntajegeneral === 11 || puntajegeneral === 1)) {
+            let valoras = parseInt(
+              prompt("sacaste un AS elije su valor solo puede ser de 1 a 11")
+            );
+            puntajegeneral = valoras;
+          }
+        }
+        break;
+    }
+    player === "cpu" ?
+      (puntoscpu += puntajegeneral) :
+      (puntoshumano += puntajegeneral);
+  }
+
+  detectarganador() {}
+}
+/**
+ *   -Ya suma correctamente los puntajes incluyendo el AS, cree el modal y programe solo falta agregar la logica 
+ * tambien estuve buscando una forma de ver que puntaje esta mas cercano a 31 y encontre algo sobre una busqueda binaria
+ * pero no entiendo si esa es la manera correcta o hay otras
+ *
+ *   -gestionar los turnos aun no puedo hacer que el banquero "tome una carta" segun le comvenga
+ *
+ *   -trabajar la parte de turnos y agregar la logica al modal para que reconozca el valor de as
+ * tambien crear la pantalla que muestra quien es el ganador y con cuantos puntos
+ *
+ */
+
 let claseui = new Ui();
 let marcador = new Marcador();
 
 class Cartas {
   constructor() {
     this._maso = [];
-  }
-
-  inicioPartida() {
-    masoDeCartas = this.armarMasoDeCartas();
-    this.barajarMaso(masoDeCartas);
-    this.tomarCartadeMaso(3, "cpu");
-    this.tomarCartadeMaso(3, "houdini");
   }
 
   armarMasoDeCartas() {
@@ -120,9 +113,9 @@ class Cartas {
     for (let i = 0; i < ncartasentregar; i++) {
       let numeroaleatorio = parseInt(Math.random() * masoDeCartas.length);
       let cartauser = masoDeCartas[numeroaleatorio];
-      player === "cpu"
-        ? (cartascpu.push(cartauser), (arraycartasplayers = cartascpu))
-        : (cartashumano.push(cartauser), (arraycartasplayers = cartashumano));
+      player === "cpu" ?
+        (cartascpu.push(cartauser), (arraycartasplayers = cartascpu)) :
+        (cartashumano.push(cartauser), (arraycartasplayers = cartashumano));
       marcador.calcularpuntos(arraycartasplayers, player);
       masoDeCartas.splice(numeroaleatorio, 1);
     }
@@ -131,21 +124,54 @@ class Cartas {
 }
 
 let claseCartas = new Cartas();
-claseCartas.inicioPartida();
+
+const inicioPartida = () => {
+  masoDeCartas = claseCartas.armarMasoDeCartas();
+  claseCartas.barajarMaso(masoDeCartas);
+  claseCartas.tomarCartadeMaso(3, "cpu");
+  claseCartas.tomarCartadeMaso(3, "humano");
+};
 
 class Jugador {
   pedirCarta(ncartasentregar, player) {
     claseCartas.tomarCartadeMaso(ncartasentregar, player);
   }
+
+  //elegir valor AS
+
+  //No pedir carta
 }
 
 class Cpu extends Jugador {
   constructor() {
     super();
   }
-  //pedir carta
+  //da bienvenida
+  //barajea cartas
+  //entrega cartas
+  //pregunta su quiere carta
+  //sino termina el juego
+}
+
+class Humano extends Jugador {
+  constructor() {
+    super();
+  }
 }
 
 document.getElementById("tomar").addEventListener("click", () => {
-  new Jugador().pedirCarta(1, "houdini");
+  new Humano().pedirCarta(1, "humano");
 });
+
+inicioPartida();
+
+//Class cartas llamara a procesos
+//procesos llama a player
+//player llama a cartas
+//
+//
+//
+//
+//si se pasa pierde,
+//si tiene 31 gana sino
+//comparar los dos puntajes solo si son menores a 31 y verifica quien tiene mayor puntaje
